0b0333c2370815662b01ffddac6001bdf6b2ca71
_Kerman <kermanx@qq.com>
2025-12-07 12:08:51 +0800

    refactor: no unsafe

--- crates/jsshaker/src/builtins/constants.rs
+++ crates/jsshaker/src/builtins/constants.rs
@@ -1,10 +1,8 @@
-use std::mem;
-
 use crate::value::ObjectId;
 
 // Builtin object ids
-pub const IMPORT_META_OBJECT_ID: ObjectId = unsafe { mem::transmute(1u32) };
-pub const REACT_NAMESPACE_OBJECT_ID: ObjectId = unsafe { mem::transmute(2u32) };
-pub const REACT_JSX_RUNTIME_NAMESPACE_OBJECT_ID: ObjectId = unsafe { mem::transmute(3u32) };
-pub const OBJECT_CONSTRUCTOR_OBJECT_ID: ObjectId = unsafe { mem::transmute(4u32) };
-pub const SYMBOL_CONSTRUCTOR_OBJECT_ID: ObjectId = unsafe { mem::transmute(5u32) };
+pub const IMPORT_META_OBJECT_ID: ObjectId = ObjectId::from_raw(1u32);
+pub const REACT_NAMESPACE_OBJECT_ID: ObjectId = ObjectId::from_raw(2u32);
+pub const REACT_JSX_RUNTIME_NAMESPACE_OBJECT_ID: ObjectId = ObjectId::from_raw(3u32);
+pub const OBJECT_CONSTRUCTOR_OBJECT_ID: ObjectId = ObjectId::from_raw(4u32);
+pub const SYMBOL_CONSTRUCTOR_OBJECT_ID: ObjectId = ObjectId::from_raw(5u32);
