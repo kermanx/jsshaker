a173207d0da260f5e0eed14cae5702447b83c82c
_Kerman <kermanx@qq.com>
2025-12-19 18:03:06 +0800

    refactor: use oxc's convertion functions

--- crates/jsshaker/src/nodes/expr/assignment_expression.rs
+++ crates/jsshaker/src/nodes/expr/assignment_expression.rs
@@ -1,6 +1,4 @@
-use oxc::ast::ast::{
-  AssignmentExpression, AssignmentOperator, BinaryOperator, Expression, LogicalOperator,
-};
+use oxc::ast::ast::{AssignmentExpression, AssignmentOperator, Expression};
 
 use crate::{
   analyzer::Analyzer, ast::AstKind2, build_effect, entity::Entity, transformer::Transformer,
@@ -63,7 +61,7 @@ impl<'a> Analyzer<'a> {
         (true, true) => {
           let left = forward_left(self);
           let right = exec_right(self);
-          self.factory.logical_result(left, right, to_logical_operator(node.operator))
+          self.factory.logical_result(left, right, node.operator.to_logical_operator().unwrap())
         }
         (false, false) => unreachable!(),
       };
@@ -76,7 +74,7 @@ impl<'a> Analyzer<'a> {
     } else {
       let (lhs, cache) = self.exec_assignment_target_read(&node.left);
       let rhs = self.exec_expression(&node.right);
-      let value = self.op_binary(to_binary_operator(node.operator), lhs, rhs);
+      let value = self.op_binary(node.operator.to_binary_operator().unwrap(), lhs, rhs);
       self.exec_assignment_target_write(&node.left, value, cache);
       value
     }
@@ -125,7 +123,7 @@ impl<'a> Transformer<'a> {
                 Some(self.ast_builder.expression_logical(
                   *span,
                   left,
-                  to_logical_operator(*operator),
+                  operator.to_logical_operator().unwrap(),
                   right,
                 ))
               } else {
@@ -139,7 +137,7 @@ impl<'a> Transformer<'a> {
             Some(self.ast_builder.expression_binary(
               *span,
               left,
-              to_binary_operator(*operator),
+              operator.to_binary_operator().unwrap(),
               right,
             ))
           }
@@ -149,32 +147,5 @@ impl<'a> Transformer<'a> {
       }
       (None, None) => None,
     }
-  }
-}
-
-fn to_logical_operator(operator: AssignmentOperator) -> LogicalOperator {
-  match operator {
-    AssignmentOperator::LogicalAnd => LogicalOperator::And,
-    AssignmentOperator::LogicalOr => LogicalOperator::Or,
-    AssignmentOperator::LogicalNullish => LogicalOperator::Coalesce,
-    _ => unreachable!(),
-  }
-}
-
-fn to_binary_operator(operator: AssignmentOperator) -> BinaryOperator {
-  match operator {
-    AssignmentOperator::Addition => BinaryOperator::Addition,
-    AssignmentOperator::Subtraction => BinaryOperator::Subtraction,
-    AssignmentOperator::Multiplication => BinaryOperator::Multiplication,
-    AssignmentOperator::Division => BinaryOperator::Division,
-    AssignmentOperator::Remainder => BinaryOperator::Remainder,
-    AssignmentOperator::Exponential => BinaryOperator::Exponential,
-    AssignmentOperator::BitwiseAnd => BinaryOperator::BitwiseAnd,
-    AssignmentOperator::BitwiseOR => BinaryOperator::BitwiseOR,
-    AssignmentOperator::BitwiseXOR => BinaryOperator::BitwiseXOR,
-    AssignmentOperator::ShiftLeft => BinaryOperator::ShiftLeft,
-    AssignmentOperator::ShiftRight => BinaryOperator::ShiftRight,
-    AssignmentOperator::ShiftRightZeroFill => BinaryOperator::ShiftRightZeroFill,
-    _ => unreachable!(),
   }
 }
